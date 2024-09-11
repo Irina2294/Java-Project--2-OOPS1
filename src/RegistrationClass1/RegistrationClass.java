f33aec99e19c61fb248f0d6fa3bd897f0019468b
Irina2294 <islivinscaia@gmail.com>
2024-09-10 21:42:52 -0500

    3rd commit

--- src/RegistrationClass1/RegistrationClass.java
+++ src/RegistrationClass1/RegistrationClass.java
@@ -1,2 +1,44 @@
-package RegistrationClass1;public class RegistrationClass {
+package RegistrationClass1;
+
+public class RegistrationClass {
+        private String email;
+        private String userName;
+        private String password;
+
+        public void setEmail(String email) {
+            if (email.endsWith("@yahoo.com")) {
+                this.email = email;
+            } else {
+                System.out.println("Invalid email. Only Yahoo emails are accepted.");
+            }
+        }
+
+        public void setUserName(String userName) {
+            if (userName != null && userName.length() > 6) {
+                this.userName = userName;
+            } else {
+                System.out.println("Invalid username. It must be non-empty and longer than 6 characters.");
+            }
+        }
+
+        public void setPassword(String password) {
+            if (password != null && password.length() > 6 && !password.contains(userName)) {
+                this.password = password;
+            } else {
+                System.out.println("Invalid password. It must be non-empty, longer than 6 characters, and cannot contain the username.");
+            }
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public String getUserName() {
+            return userName;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+
 }
--- src/RegistrationClass1/RegistrationTester.java
+++ src/RegistrationClass1/RegistrationTester.java
@@ -1,2 +1,15 @@
-package RegistrationClass1;public class RegistrationTester {
-}
+package RegistrationClass1;
+
+public class RegistrationTester {
+        public static void main(String[] args) {
+            RegistrationClass reg = new RegistrationClass();
+
+            reg.setEmail("user1@yahoo.com");
+            reg.setUserName("username22");
+            reg.setPassword("password1");
+
+            System.out.println("Email: " + reg.getEmail());
+            System.out.println("Username: " + reg.getUserName());
+            System.out.println("Password: " + reg.getPassword());
+        }
+    }
